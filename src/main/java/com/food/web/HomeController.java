d34a1965b29b64f968b32d9207f381979127a89c
sangcheolkim <tkdral@naver.com>
2017-09-22 17:08:36 +0900

    Controller Change

--- src/main/java/com/food/web/HomeController.java
+++ src/main/java/com/food/web/HomeController.java
@@ -41,4 +41,14 @@ public class HomeController {
 		return "main";
 	}
 	
+	@RequestMapping(value="/main2", method= RequestMethod.GET)
+	public String main2(Model model){
+		return "main2";
+	}
+	
+	@RequestMapping(value="/main_plate_main", method= RequestMethod.GET)
+	public String main3(Model model){
+		return "main_plate_main";
+	}
+	
 }
