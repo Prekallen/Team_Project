723b9437cfd4674daed2a1a2d1469e63336a96b7
sangcheolkim <tkdral@naver.com>
2017-09-27 18:37:39 +0900

    google api(incomplete)

--- src/main/java/com/food/web/google/controller/GoogleController.java
+++ src/main/java/com/food/web/google/controller/GoogleController.java
@@ -1,8 +1,15 @@
 package com.food.web.google.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.food.web.google.dto.GoogleInfo;
 import com.food.web.google.service.GoogleService;
 
 @Controller
@@ -10,4 +17,9 @@ public class GoogleController {
 	
 	@Autowired
 	GoogleService gs;
+	
+//	@RequestMapping(value="test/test", method=RequestMethod.POST)
+//	public @ResponseBody List<GoogleInfo> getGoodsInfoList(@RequestBody String query){
+//		return gs.getGoogleInfoList(query);
+//	}
 }
