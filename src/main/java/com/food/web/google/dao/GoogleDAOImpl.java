703a834cc58f2ddf65ce885f4c7ebfd4a0145105
sangcheolkim <tkdral@naver.com>
2017-09-28 12:25:44 +0900

    Annotation setting

--- src/main/java/com/food/web/google/dao/GoogleDAOImpl.java
+++ src/main/java/com/food/web/google/dao/GoogleDAOImpl.java
@@ -1,5 +1,8 @@
 package com.food.web.google.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class GoogleDAOImpl implements GoogleDAO{
 
 }
--- src/main/java/com/food/web/google/service/GoogleServiceImpl.java
+++ src/main/java/com/food/web/google/service/GoogleServiceImpl.java
@@ -1,9 +1,10 @@
 package com.food.web.google.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.food.web.google.dao.GoogleDAO;
-
+@Service
 public class GoogleServiceImpl implements GoogleService{
 	
 	@Autowired
